6e44f1165ff88915828aeabc58c86b130a7e11cc
hyunsoo3075 <55557666+hyunsoo3075@users.noreply.github.com>
2019-12-05 04:19:47 -0500

    added comments

--- src/Ball1.java
+++ src/Ball1.java
@@ -1,32 +1,60 @@
 import java.awt.Color;
 
+/**
+ * getters and setters for the ball characteristics
+ */
 public class Ball1 implements GameObject {
     private double x = -15;
     private double y = -15;
     private double width = 10;
     private double height = 10;
-    
+
+    /**
+     * gets the x coordinate of the ball
+     * @return the x coordinate
+     */
     public double getX(){
         return x;
     }
-    
+
+    /**
+     * gets the y coordinate of the ball
+     * @return the y coordinate
+     */
     public double getY(){
         return y;
     }
-    
+
+    /**
+     * gets the width of the ball for the hitbox
+     * @return the width of the ball
+     */
     public double getWidth(){
         return width;
     }
-    
+
+    /**
+     * gets the height of the ball for the hit box
+     * @return the height of the bal
+     */
     public double getHeight(){
         return height;
     }
-    
+
+    /**
+     * sets the ball somewhere
+     * @param newX x coordinate
+     * @param newY y coordinate
+     */
     void setCoordinates(double newX, double newY){
         x = newX;
         y = newY;
     }
-    
+
+    /**
+     * color of the ball
+     * @return the color
+     */
     Color getColor() {
     	return Color.RED;
     }
